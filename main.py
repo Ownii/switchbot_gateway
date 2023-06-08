67a510ba577c29e749734649ecdc15baf61b8a9a
Martin Förster <martinfoerster95@gmx.de>
2023-06-08 15:16:53 +0200

    scan just one

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from switchbotmeter import DevScanner
 import requests
 
-for current_devices in DevScanner():
-    for device in current_devices:
-        data = {
-            'mac': device.mac,
-            'model': device.model,
-            'mode': device.mode,
-            'date': str(device.date),
-            'temp': device.temp,
-            'humidity': device.humidity,
-        }
-        requests.post('http://192.168.178.89:8091/switchbot', json=data)
-        print("===========")
-        print(f'mac: {device.mac}')
-        print(f'model: {device.model}')
-        print(f'mode: {device.mode}')
-        print(f'date: {device.date}')
-        print(f'temp: {device.temp}')
-        print(f'humidity: {device.humidity}')
-        print("===========")
-        print()
-        print()
+current_devices = next(DevScanner())
+for device in current_devices:
+    data = {
+        'mac': device.mac,
+        'model': device.model,
+        'mode': device.mode,
+        'date': str(device.date),
+        'temp': device.temp,
+        'humidity': device.humidity,
+    }
+    requests.post('http://192.168.178.89:8091/switchbot', json=data)
+    print("===========")
+    print(f'mac: {device.mac}')
+    print(f'model: {device.model}')
+    print(f'mode: {device.mode}')
+    print(f'date: {device.date}')
+    print(f'temp: {device.temp}')
+    print(f'humidity: {device.humidity}')
+    print("===========")
+    print()
+    print()
