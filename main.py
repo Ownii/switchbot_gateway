154926d2cc5e75bcdbfeb41068f3926595b87738
Martin Förster <martinfoerster95@gmx.de>
2023-06-08 14:14:36 +0200

    debug 2

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from switchbotmeter import DevScanner
 
 for current_devices in DevScanner():
-    for device in current_devices:
+    print("test")
+#    for device in current_devices:
         #print(device)
-        print("found device")
-        print(device.mac)
-        print(device.temp)
+        #print("found device")
+        #print(device.mac)
+        #print(device.temp)
         #print(f'{device.mac} -> {device.temp}')
