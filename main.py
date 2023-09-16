0e1733af9c71032e2f6e5855289f4a9d5186265b
Martin Förster <martinfoerster95@gmx.de>
2023-09-16 20:13:17 +0200

    Revert "get port as int"
    This reverts commit 50baa30d065f1248de963de032e0e364e07bf850.

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from switchbotmeter import DevScanner
 import requests
 import configparser
-import paho.mqtt.client as mqtt
 
 config = configparser.ConfigParser()
 config.read("config.ini")
-client = mqtt.Client()
-
-if config["MQTT"] is not None:
-    client.connect(config["MQTT"]["SERVER"], config["MQTT"].getint("PORT"), 60)
 
 current_devices = next(DevScanner())
 for device in current_devices:
@@ -21,8 +16,6 @@ for device in current_devices:
         'humidity': device.humidity,
     }
     requests.post(config["API"]["DATA_ENDPOINT"], json=data)
-    if client is not None:
-        client.publish("switchbot", data)
     print("===========")
     print(f'mac: {device.mac}')
     print(f'model: {device.model}')
